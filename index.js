22648f93f4eb61ce2d804990f3e96f760f0c3854
xxxFilosoFxxx <roman.lashnev@mail.ru>
2020-11-15 23:33:19 +0300

    v 1.1

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-var express = require('express');
+const express = require("express");
+const app = express();
 
-var app = express();
+// redirect bootstrap JS
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
+// redirect JS jQuery
+app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"));
+// redirect CSS bootstrap
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+// redirect CSS and JS
+app.use("/public", express.static(__dirname + "/public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
-    var obj = {title: "Название"}
-    res.render('index', {obj: obj});
+app.get("/", function(req, res) {
+  res.render("index", {
+    title: "Название",
+  });
 });
 
-app.listen(8000);
+app.listen(8000, function() {
+  console.log("Example app listening on port 8000!");
+});
+
