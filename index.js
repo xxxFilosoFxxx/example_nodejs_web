7a70b12f37b8d8aa0091cb5b71a24a3c52452620
xxxFilosoFxxx <roman.lashnev@mail.ru>
2020-11-15 22:15:42 +0300

    v 1.0

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-var fs = require('fs');
-var http = require('http');
+var express = require('express');
 
-var server = http.createServer(function(req, res) {
-    console.log("Url: " + req.url);
-    if (req.url === '/index' || req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(res);
-    }
-    else {
-        res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
-        fs.createReadStream(__dirname + '/404.html', 'utf8').pipe(res);
-    }
+var app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    var obj = {title: "Название"}
+    res.render('index', {obj: obj});
 });
 
-server.listen(8000, '127.0.0.1');
-console.log("Port: 8000");
-
-
-
+app.listen(8000);
