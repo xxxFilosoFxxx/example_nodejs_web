0c8524ba8dd0dcd5d668f712f35795f71f433a0a
xxxFilosoFxxx <roman.lashnev@mail.ru>
2020-11-18 04:29:41 +0300

    v1.6

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,25 @@
 $(function() {
+  // TODO: fix scroll
+  // const windowHeight = $(window).height();
 
-    const currentNumber_1 = $('#dynamic-number-1').text();
+  // $(document).on("scroll", function() {
+  //   $(".counter").each(function() {
+  //     const self = $(this);
+  //     const height = self.offset().top + self.height();
+  //     if ($(document).scrollTop() + windowHeight >= height) {
+  //       self.addClass("dynamic-number");
+  //     }
+  //   });
+  // });
 
-    $('#dynamic-number-1').animate({numberValue: currentNumber_1}, {
-        duration: 3000,
-        step: function() {
-            $('#dynamic-number-1').text(Math.ceil(this.numberValue));
-        }
+
+  $(".dynamic-number").each( function(index, value) {
+    $(this).animate({numberValue: $(this).text()}, {
+      duration: 3000,
+      step: function() {
+        $(this).text(Math.ceil(this.numberValue));
+      },
     });
-
-
-    const currentNumber_2 = $('#dynamic-number-2').text();
-
-    $('#dynamic-number-2').animate({numberValue: currentNumber_2}, {
-        duration: 3000,
-        step: function() {
-            $('#dynamic-number-2').text(Math.ceil(this.numberValue));
-        }
-    });
-
-
-    const currentNumber_3 = $('#dynamic-number-3').text();
-
-    $('#dynamic-number-3').animate({numberValue: currentNumber_3}, {
-        duration: 3000,
-        step: function() {
-            $('#dynamic-number-3').text(Math.ceil(this.numberValue));
-        }
-    });
-
-
-    const currentNumber_4 = $('#dynamic-number-4').text();
-
-    $('#dynamic-number-4').animate({numberValue: currentNumber_4}, {
-        duration: 3000,
-        step: function() {
-            $('#dynamic-number-4').text(Math.ceil(this.numberValue));
-        }
-    });
+  });
 
 });
